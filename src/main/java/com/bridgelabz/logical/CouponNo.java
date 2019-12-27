1fc86fbca363f24676e3b0539815fb16fd82ed84
explorehorizon15 <58970498+explorehorizon15@users.noreply.github.com>
2019-12-27 16:02:24 +0530

    Update CouponNo.java

--- src/main/java/com/bridgelabz/logical/CouponNo.java
+++ src/main/java/com/bridgelabz/logical/CouponNo.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class CouponNo {
 
     public static int couponNo() {
-        int cn, n;
+         	 Set<Object> hs= new HashSet<Object>();
+              int cn, n;
        
         Scanner src = new Scanner(System.in);
         n = src.nextInt();
         for (int j = 0; j < n; j++) {
             cn = (int) (Math.random() * n);
-            System.out.println("Coupon No=" + cn);
+            hs.add(cn);
 
+        }
+         Iterator itr=hs.iterator();
+        while(itr.hasNext()) {
+        	System.out.println(itr.next());
         }
         src.close();
         return n;
