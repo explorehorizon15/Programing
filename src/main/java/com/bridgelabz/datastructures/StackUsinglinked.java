1a3a09d687a1cc3be598c113802a7be83dbafe04
Sayyada29 <prasadpalkar94@gmail.com>
2020-01-07 20:17:31 +0530

    [prasad]stull

--- src/main/java/com/bridgelabz/datastructures/StackUsinglinked.java
+++ src/main/java/com/bridgelabz/datastructures/StackUsinglinked.java
@@ -1,92 +1,70 @@
 package com.bridgelabz.datastructures;
 
-
-
 public class StackUsinglinked {
-	public class Node{
+	public class Node {
 		int data;
 		Node next;
 	}
+
 	Node top;
-	int size=0;
+
+	StackUsinglinked() {
+		this.top = null;
+	}
+
 	public void push(int data) {
-		Node node= new Node();
-		node.data=data;
-		node.next=null;
-		if(top==null) {
-			top=node;
-		}else {
-			Node n=top;
-			while(n.next!=null) {
-				n=n.next;
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		if (top == null) {
+			top = node;
+		} else {
+			Node n = top;
+			while (n.next != null) {
+				n = n.next;
 			}
-		n.next=node;
-		size++;
+			n.next = node;
+
 		}
 	}
-	
-	public void pop(int index) {
-		if(top==null) {
+
+	public void pop() {
+		if (top == null) {
 			System.out.println("Stack is Empty!!");
-		}else {
-		if(index==0) {
-			top=top.next;	
-		}else {
-			Node n=top;
-			Node n1=null;
-			for(int i=0;i<=index-1;i++) {
-				n=n.next;
-				
+		} else {
+			Node n = top;
+			Node prev = null;
+			while (n.next != null) {
+				prev = n;
+				n = n.next;
 			}
-			size--;
-			n1=n.next;
-			n.next=n1.next;
-			
+			System.out.println("Popped data is:" + prev.next.data);
+			prev.next = null;
 		}
-		}
-		
-		
 	}
-	
+
 	public void show() {
-		Node node=top;
-		while(node.next!=null) {
+		Node node = top;
+		while (node.next != null) {
 			System.out.println(node.data);
-			node=node.next;
-				
+			node = node.next;
 		}
 		System.out.println(node.data);
 	}
-	
-	
-	public boolean isEmpty() {
-		if(size>0) {
-			return false;
-		}
-		return true;
-	}
-	  
-	public int getSize() {
-		return size;
-	}
 
 	public static void main(String[] args) {
-		StackUsinglinked obj= new StackUsinglinked();
-		System.out.println(obj.isEmpty());
-		obj.push(15);
-		obj.push(29);
-		obj.push(26);
-		obj.push(35);
-		obj.push(39);
-		obj.push(46);
-		
-		obj.show();
-		System.out.println(obj.getSize());
-		System.out.println(obj.isEmpty());
-		System.out.println("Popped:");
-		obj.pop(1);
-		obj.show();
-		
-	}
+		StackUsinglinked obj = new StackUsinglinked();
+		obj.pop();
+		obj.push(1);
+		obj.push(2);
+		obj.push(3);
+		obj.push(4);
 
+		obj.show();
+		obj.pop();
+		obj.pop();
+		System.out.println("after popped:");
+		obj.show();
+
+	}
 }
